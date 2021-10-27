faf0631be428bef5e215c66c642598a285874400
Rose Griffin <60629645+rosegriffin@users.noreply.github.com>
2021-10-26 22:05:03 -0400

    Pass by reference

--- tests/phon_rules.cpp
+++ tests/phon_rules.cpp
@@ -5,8 +5,11 @@
 
 #include "../ipa/ipa.h"
 
-static std::vector<SoundSymbol> ruleVoicing(std::map<std::string, SoundSymbol> soundSymbols, std::vector<SoundSymbol> input);
-static std::string getRepresentation(std::vector<SoundSymbol> input);
+static std::vector<SoundSymbol> ruleVoicing(
+    std::map<std::string, SoundSymbol> * const soundSymbols,
+    std::vector<SoundSymbol> * const input);
+
+static std::string getRepresentation(std::vector<SoundSymbol> * const input);
 
 int main() {
     std::map<std::string, SoundSymbol> soundSymbols;
@@ -39,12 +42,17 @@ int main() {
         soundSymbols["i"]
     };
 
-    std::cout << "/" << getRepresentation(word1) << "/ --> ["
-              << getRepresentation(ruleVoicing(soundSymbols, word1))  << "]" << std::endl
-              << "/" << getRepresentation(word2) << "/ --> ["
-              << getRepresentation(ruleVoicing(soundSymbols, word2))  << "]" << std::endl
-              << "/" << getRepresentation(word3) << "/ --> ["
-              << getRepresentation(ruleVoicing(soundSymbols, word3))  << "]" << std::endl;
+    // Phonetic Representation
+    std::vector<SoundSymbol> rep1 = ruleVoicing(&soundSymbols, &word1); //skædz
+    std::vector<SoundSymbol> rep2 = ruleVoicing(&soundSymbols, &word2); //tætsʌs
+    std::vector<SoundSymbol> rep3 = ruleVoicing(&soundSymbols, &word3); //soʊgzdi
+
+    std::cout << "/" << getRepresentation(&word1) << "/ --> ["
+              << getRepresentation(&rep1)  << "]" << std::endl
+              << "/" << getRepresentation(&word2) << "/ --> ["
+              << getRepresentation(&rep2)  << "]" << std::endl
+              << "/" << getRepresentation(&word3) << "/ --> ["
+              << getRepresentation(&rep3)  << "]" << std::endl;
 
     return 0;
 }
@@ -52,12 +60,12 @@ int main() {
 /* Given a vector of sound symbols, return its
  * string representation
  */
-static std::string getRepresentation(std::vector<SoundSymbol> input) {
+static std::string getRepresentation(std::vector<SoundSymbol> * const input) {
     std::string output = "";
-    int len = input.size();
+    int len = (*input).size();
 
     for (int i = 0; i < len; i++) {
-        output += input[i].getSymbol();
+        output += (*input)[i].getSymbol();
     }
     return output;
 }
@@ -65,22 +73,25 @@ static std::string getRepresentation(std::vector<SoundSymbol> input) {
 /* Voiceless alveolar fricative becomes voiced if it occurs after a voiced consonant
  * /s/ --> [z] / C_
  */
-static std::vector<SoundSymbol> ruleVoicing(std::map<std::string, SoundSymbol> soundSymbols, std::vector<SoundSymbol> input) {
+static std::vector<SoundSymbol> ruleVoicing(
+    std::map<std::string, SoundSymbol> * const soundSymbols,
+    std::vector<SoundSymbol> * const input) {
+
     std::vector<SoundSymbol> output;
-    int len = input.size();
+    int len = (*input).size();
 
     for (int i = 0; i < len; i++) {
         // Check if current symbol is a voiceless alveolar fricative,
         // if there is a symbol before it,
         // if the preceeding symbol is a consonant,
         // and if the preceeding symbol is voiced
-        if (input[i].getSymbol() == "s" && (i - 1) >= 0
-            && input[i - 1].getType() == CONSONANT
-            && input[i - 1].isSymVoiced() == true) {
+        if ((*input)[i].getSymbol() == "s" && (i - 1) >= 0
+            && (*input)[i - 1].getType() == CONSONANT
+            && (*input)[i - 1].isSymVoiced() == true) {
 
-            output.push_back(soundSymbols["z"]);
+            output.push_back((*soundSymbols)["z"]);
         } else {
-            output.push_back(input[i]);
+            output.push_back((*input)[i]);
         }
     }
     return output;
